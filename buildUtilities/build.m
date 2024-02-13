adc8617ce5ff45cfad5b403c1425ed7dceccfd0d
mic <mic@mic-pc>
2024-02-13 22:53:23 +0100

    improved build.m and build version 0.1.0.6

--- buildUtilities/build.m
+++ buildUtilities/build.m
@@ -7,12 +7,13 @@ toolboxPackagingConfFullPath = fullfile(toolboxPackagingConfDir,'ToolboxPackagin
 % Get current version 
 currentVersion = matlab.addons.toolbox.toolboxVersion(toolboxPackagingConfFullPath);
 currentVersionNumber = str2double(extract(currentVersion,digitsPattern));  
+disp("Previous version: " + currentVersion);
 
 % Create new version number
 newVersionNumber = currentVersionNumber;
 newVersionNumber(4) = newVersionNumber(4) + 1; 
 newVersionString = join(string(newVersionNumber),".");
-
+disp("New version: " + newVersionString);
 
 % insert new version in the toolbox configuration
 matlab.addons.toolbox.toolboxVersion(toolboxPackagingConfFullPath,newVersionString);
@@ -21,3 +22,5 @@ matlab.addons.toolbox.toolboxVersion(toolboxPackagingConfFullPath,newVersionStri
 % package toolbox  
 newfileName = fullfile(toolboxPackagingConfDir,"releases","kview_v" + newVersionString);
 matlab.addons.toolbox.packageToolbox(toolboxPackagingConfFullPath,newfileName);
+
+disp("-- Build completed --");
