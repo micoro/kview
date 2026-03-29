39f33e7a5213b6106714dee2e267d01996c5fa0c
mic <mic@cachyos-mic>
2026-03-29 12:41:23 +0200

    moveElementInArray: new output available kvsettingsGUI: improved, now the order of data in the table can be reorganized

--- toolbox/support functions/moveElementInArray.m
+++ toolbox/support functions/moveElementInArray.m
@@ -1,10 +1,14 @@
-function outputArray = moveElementInArray(inputArray,listOfIndexToMove,direction)
+function [outputArray, listOfIndexToMoveNewPosition] = moveElementInArray(inputArray,listOfIndexToMove,direction)
 %MOVEELEMENTINARRAY move a list of elements in an array up and down by one
 %
 % DESCRIPTION
 % This function can be use to move one (or more, even non contiguous)
 % elements of an array up and down the array by one unit.
 % If an element is at the end of the array it cannot move.
+%
+% OUTPUT:
+%   outputArray                     the inputArray but reordered by the function
+%   listOfIndexToMoveNewPosition    the new position that the listOfIndexToMove have reached 
 
 
 arguments
@@ -15,19 +19,27 @@ end
 
 indexList = 1:numel(inputArray); 
 listOfIndexToMove = indexList(listOfIndexToMove); % in case listOfIndexToMove is a logical array it will be transformed into the actual index number
-
+listOfIndexToMoveNewPosition = [];
     
 if direction == "up"
     for indexToMove = listOfIndexToMove
-        if indexToMove == 1 || any(listOfIndexToMove == indexList(indexToMove-1)); continue; end
+        if indexToMove == 1 || any(listOfIndexToMove == indexList(indexToMove-1))
+            listOfIndexToMoveNewPosition(end+1) = indexToMove;
+            continue; 
+        end
         indexList = [indexList(1:indexToMove-2) indexList(indexToMove) indexList(indexToMove-1) indexList(indexToMove+1:end)];
+        listOfIndexToMoveNewPosition(end+1) = indexToMove-1;
     end
 end
 
 if direction == "down"
     for indexToMove = flip(listOfIndexToMove)
-        if indexToMove == numel(indexList) || any(listOfIndexToMove == indexList(indexToMove+1)); continue; end
+        if indexToMove == numel(indexList) || any(listOfIndexToMove == indexList(indexToMove+1))
+            listOfIndexToMoveNewPosition(end+1) = indexToMove;
+            continue; 
+        end
         indexList = [indexList(1:indexToMove-1) indexList(indexToMove+1) indexList(indexToMove) indexList(indexToMove+2:end)];
+        listOfIndexToMoveNewPosition(end+1) = indexToMove+1;
     end
 end
 
